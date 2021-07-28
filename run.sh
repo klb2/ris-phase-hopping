4aad8037e3c97e65a9f58a7bd7364f50d41bfeb9
KLB <k.besser@tu-bs.de>
2021-07-28 14:15:07 +0200

    Add more commands to reproduce figures

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
-# Run all script to generate the results presented in the paper "Artificial
-# Fast Fading from Reconfigurable Surfaces Enables Ultra-Reliable
-# Communications" (Eduard Jorswieck, Karl-L. Besser, Cong Sun).
+# Run all script to generate the results presented in the paper "Reconfigurable
+# Intelligent Surface Phase Hopping for Ultra-Reliable Communications" (Karl-L.
+# Besser, Eduard Jorswieck).
 #
 # Copyright (C) 2021 Karl-Ludwig Besser
 # License: GPLv3
 
-echo "Running outage probability for constant phases..."
-echo "Figure 2: NLOS Scenario"
+echo "Figure 2: Outage probability constant phases NLOS Scenario"
 python3 constant_phases.py -a 0 -N 4 10 50 -n 1000000 --plot
-echo "Figure 3: LOS Scenario with a=2"
+
+echo "Figure 3: Outage probability constant phases LOS Scenario with a=2"
 python3 constant_phases.py -a 2 -N 4 10 50 -n 1000000 --plot
 
 echo "Figure 4: Calculating ergodic capacities for NLOS scenario..."
@@ -21,8 +21,8 @@ python3 random_phases.py -a 0 -N 4 20 50 -s 1000 -f 5000 --plot
 echo "Figure 6: Outage probability log-plot..."
 python3 random_phases.py -a 0 -N 4 -s 100000 -f 2000 --logplot
 
-#echo "Running two-element example with random phases..."
-#python3 random_phases.py -N 2 -s 1000 -f 5000 --plot
-#
-#echo "Running two-element example with quantized phases..."
-#python3 discrete_phases.py -N 2 -K 2 -s 1000 -f 5000 --plot
+echo "Figure 7: Calculating ergodic capacities for LOS scenario..."
+python3 ergodic_capacity.py -a 0 2 4 -N {4..30} --plot
+
+echo "Figure 8: Outage probability for LOS sceneario..."
+python3 random_phases.py -a 2 -N 4 10 50 -s 1000 -f 5000 --plot
