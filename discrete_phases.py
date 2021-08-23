e0bcbbff6ff99615a3aab89917c7903bc1386452
KLB <k.besser@tu-bs.de>
2021-08-23 15:09:20 +0200

    Update discrete phases script

--- discrete_phases.py
+++ discrete_phases.py
@@ -3,39 +3,52 @@ import matplotlib.pyplot as plt
 from scipy import stats, special
 
 from phases import gains_constant_phase, rvs_channel_phases, rvs_ris_phases_quant
+from utils import export_results
 
 def discrete_rvs_phases(num_elements, num_phase_steps, num_samples_slow=1000,
-                        num_samples_fast=5000, plot=False):
+                        num_samples_fast=5000, plot=False, export=False):
     if plot:
         fig, axs = plt.subplots()
-    dependency = "indep"
     channel_realizations = rvs_channel_phases(num_elements, num_samples_slow)
     channel_realizations = np.tile(channel_realizations, (num_samples_fast, 1, 1))
-    ris_phases = rvs_ris_phases_quant(num_elements, num_samples_slow,
-                                      num_samples_fast, copula=dependency,
-                                      K=num_phase_steps)
-    total_phases = channel_realizations + ris_phases
-    const_phase = gains_constant_phase(total_phases)
-    capac_const_phase = np.log2(1 + const_phase)
-    expect_capac = np.mean(capac_const_phase, axis=0)
-    _hist = np.histogram(expect_capac, bins=100)
-    _r_ax = np.linspace(0, 3, 1000)
-    cdf_hist = stats.rv_histogram(_hist).cdf(_r_ax)
-    #cdf_exact = cdf_ergodic_capac(_r_ax, copula=dependency)
+    results = {}
+    #_r_ax = np.linspace(0.5, 4, 2000)
+    _r_ax = np.linspace(0, 3, 2000)
+    for _K in num_phase_steps:
+        ris_phases = rvs_ris_phases_quant(num_elements, num_samples_slow,
+                                          num_samples_fast, copula="indep",
+                                          K=_K)
+        total_phases = channel_realizations + ris_phases
+        const_phase = gains_constant_phase(total_phases)
+        capac_const_phase = np.log2(1 + const_phase)
+        expect_capac = np.mean(capac_const_phase, axis=0)
+        print("K={:d}: ZOC={:.3f}".format(_K, min(expect_capac)))
+        #_r_ax = np.linspace(.9*min(expect_capac), 1.1*max(expect_capac), 500)
+        _hist = np.histogram(expect_capac, bins=100)
+        cdf_hist = stats.rv_histogram(_hist).cdf(_r_ax)
+        _erg_cap_appr = -np.exp(1/num_elements)*special.expi(-1/num_elements)/np.log(2)
+        cdf_appr = np.heaviside(_r_ax-_erg_cap_appr, 0)
+        results["ecdf{:d}".format(_K)] = cdf_hist
+        results["appr{:d}".format(_K)] = cdf_appr
+        if plot:
+            axs.plot(_r_ax, cdf_hist, label="K={:d}".format(_K))
     if plot:
-        axs.plot(_r_ax, cdf_hist, label="ECDF -- K={:d}".format(num_phase_steps))
-        #axs.plot(_r_ax, cdf_exact, '--', label="Exact -- {}".format(dependency))
         axs.legend()
-        axs.set_title("Artificial Fast Fading with N={:d} RIS Elements\nQuantized Phases with {:d} Steps".format(num_elements, num_phase_steps))
+        axs.set_title("Artificial Fast Fading with N={:d} RIS Elements\nQuantized Phases".format(num_elements))
         axs.set_xlabel("Rate $R$")
         axs.set_ylabel("Outage Probability")
+
+    if export:
+        results["rate"] = _r_ax
+        export_results(results, "quant-phase-N{:d}".format(num_elements))
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("--plot", action="store_true")
+    parser.add_argument("--export", action="store_true")
     parser.add_argument("-N", "--num_elements", type=int, default=2)
-    parser.add_argument("-K", "--num_phase_steps", type=int, default=2)
+    parser.add_argument("-K", "--num_phase_steps", type=int, nargs="+", default=[2, 3, 5, 10])
     parser.add_argument("-f", "--num_samples_fast", type=int, default=5000)
     parser.add_argument("-s", "--num_samples_slow", type=int, default=1000)
     args = vars(parser.parse_args())
