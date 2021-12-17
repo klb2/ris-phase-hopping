123c23c572d5d9aff6a05bbd9b09b8fda8de446d
KLB <k.besser@tu-bs.de>
2021-12-17 12:08:40 +0100

    Add connection probability to quantized phase script

--- discrete_phases.py
+++ discrete_phases.py
@@ -5,21 +5,24 @@ from scipy import stats, special
 from phases import gains_constant_phase, rvs_channel_phases, rvs_ris_phases_quant
 from utils import export_results
 
-def discrete_rvs_phases(num_elements, num_phase_steps, num_samples_slow=1000,
-                        num_samples_fast=5000, plot=False, export=False):
+def discrete_rvs_phases(num_elements, num_phase_steps, connect_prob,
+                        num_samples_slow=1000, num_samples_fast=5000,
+                        plot=False, export=False):
     if plot:
         fig, axs = plt.subplots()
     channel_realizations = rvs_channel_phases(num_elements, num_samples_slow)
     channel_realizations = np.tile(channel_realizations, (num_samples_fast, 1, 1))
+    channel_absolute = stats.bernoulli.rvs(p=connect_prob, size=(num_samples_slow, num_elements))
+    channel_absolute = np.tile(channel_absolute, (num_samples_fast, 1, 1))
     results = {}
     #_r_ax = np.linspace(0.5, 4, 2000)
-    _r_ax = np.linspace(0, 3, 2000)
+    _r_ax = np.linspace(0, 5, 2000)
     for _K in num_phase_steps:
         ris_phases = rvs_ris_phases_quant(num_elements, num_samples_slow,
                                           num_samples_fast, copula="indep",
                                           K=_K)
         total_phases = channel_realizations + ris_phases
-        const_phase = gains_constant_phase(total_phases)
+        const_phase = gains_constant_phase(total_phases, path_amp=channel_absolute)
         capac_const_phase = np.log2(1 + const_phase)
         expect_capac = np.mean(capac_const_phase, axis=0)
         print("K={:d}: ZOC={:.3f}".format(_K, min(expect_capac)))
@@ -40,7 +43,7 @@ def discrete_rvs_phases(num_elements, num_phase_steps, num_samples_slow=1000,
 
     if export:
         results["rate"] = _r_ax
-        export_results(results, "quant-phase-N{:d}".format(num_elements))
+        export_results(results, "quant-phase-N{:d}-p{:.3f}".format(num_elements, connect_prob))
 
 if __name__ == "__main__":
     import argparse
@@ -51,6 +54,7 @@ if __name__ == "__main__":
     parser.add_argument("-K", "--num_phase_steps", type=int, nargs="+", default=[2, 3, 5, 10])
     parser.add_argument("-f", "--num_samples_fast", type=int, default=5000)
     parser.add_argument("-s", "--num_samples_slow", type=int, default=1000)
+    parser.add_argument("-p", "--connect_prob", type=float)#, nargs="+", default=[1.])
     args = vars(parser.parse_args())
     discrete_rvs_phases(**args)
     plt.show()
